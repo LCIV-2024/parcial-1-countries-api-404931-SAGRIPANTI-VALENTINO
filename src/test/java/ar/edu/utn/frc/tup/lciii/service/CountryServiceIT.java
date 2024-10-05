87f2d985978ea8a4d8420e54a5fc04ce2c0a2567
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-05 15:46:22 -0300

    test integracion

---
+++ src/test/java/ar/edu/utn/frc/tup/lciii/service/CountryServiceIT.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.frc.tup.lciii.service;
+
+import ar.edu.utn.frc.tup.lciii.model.Country;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class CountryServiceIT {
+    @Autowired
+    private CountryService countryService;
+    @Test
+    void getAllCountries() {
+        List<Country> response = countryService.getAllCountries();
+        assertNotNull(response);
+        assertEquals(250, response.size());
+    }
+}
---
+++ src/test/java/ar/edu/utn/frc/tup/lciii/service/CountryServiceTest.java
@@ -0,0 +1,40 @@
+package ar.edu.utn.frc.tup.lciii.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryServiceTest {
+
+    @Test
+    void getAllCountries() {
+    }
+
+    @Test
+    void mapToDTOList() {
+    }
+
+    @Test
+    void getCountryByName() {
+    }
+
+    @Test
+    void getCountryByCode() {
+    }
+
+    @Test
+    void getContriesByContinent() {
+    }
+
+    @Test
+    void getCountriesByLanguage() {
+    }
+
+    @Test
+    void getCountryMostBorders() {
+    }
+
+    @Test
+    void saveCountries() {
+    }
+}
